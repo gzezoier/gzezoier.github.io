f74e146a3740358075e44c9339dbc030e356f912
szdytom <szdytom@163.com>
2019-08-25 16:34:19 +0800

    fix -1 ???
    Signed-off-by: szdytom <szdytom@163.com>

--- aichess/script.js
+++ aichess/script.js
@@ -11,6 +11,9 @@ var minimaxRoot =function(depth, game, isMaximisingPlayer) {
 
     var newGameMoves = game.ugly_moves();
     var bestMove = -9999;
+
+    if (!isMaximisingPlayer) bestMove = 9999;
+
     var bestMoveFound;
 
     for(var i = 0; i < newGameMoves.length; i++) {
@@ -18,9 +21,17 @@ var minimaxRoot =function(depth, game, isMaximisingPlayer) {
         game.ugly_move(newGameMove);
         var value = minimax(depth - 1, game, -10000, 10000, !isMaximisingPlayer);
         game.undo();
-        if(value >= bestMove) {
-            bestMove = value;
-            bestMoveFound = newGameMove;
+
+        if (isMaximisingPlayer) {
+            if(value >= bestMove) {
+                bestMove = value;
+                bestMoveFound = newGameMove;
+            }
+        } else {
+            if(value <= bestMove) {
+                bestMove = value;
+                bestMoveFound = newGameMove;
+            }
         }
     }
     return bestMoveFound;
